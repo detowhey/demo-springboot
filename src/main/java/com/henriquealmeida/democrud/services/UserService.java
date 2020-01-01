55bc4af4e7909d8aa933873c96cce9eebb8be23f
detowhey <henriquefr.almeida@gmail.com>
2020-01-01 16:36:58 -0300

    Service layer, component registration

---
+++ src/main/java/com/henriquealmeida/democrud/services/UserService.java
@@ -0,0 +1,26 @@
+package com.henriquealmeida.democrud.services;
+
+import java.util.List;
+import java.util.Optional;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+
+import com.henriquealmeida.democrud.entities.User;
+import com.henriquealmeida.democrud.repositories.UserRepository;
+
+@Service
+public class UserService {
+	
+	@Autowired
+	private UserRepository repository;
+	
+	public List<User> findAll(){
+		return repository.findAll();
+	}
+	
+	public User findById (long id) {
+		Optional<User> obj = repository.findById(id);
+		return obj.get();
+	}
+}
